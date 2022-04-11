b9a7877d73d3a59a1213a80375965b21e8d5d5ca
PhaserEditor2D <developers@phasereditor2d.com>
2022-04-11 14:21:48 -0400

    Make apps data scripts.

---
+++ make-apps-data.js
@@ -0,0 +1,37 @@
+import { readFileSync, writeFileSync } from "fs";
+
+console.log("Start making data for offline apps");
+
+const data = JSON.parse(readFileSync("www/data.json"));
+
+
+for (const kind of ["starters", "examples"]) {
+
+    const inputData = data.$pages.find(p => p.$name === kind);
+
+    const outputData = [];
+
+    for (const itemData of inputData.$pages) {
+
+        const {
+            $name,
+            title,
+            description,
+            image,
+            zip_url,
+            tags } = itemData;
+
+        outputData.push({
+            path: `${kind}/${$name}`,
+            title,
+            description,
+            image: `${kind}/${$name}/${image}`,
+            zip_url,
+            tags
+        });
+    }
+
+    console.log(`Writing to www/${kind}-data.json`);
+    writeFileSync(`www/${kind}-data.json`, JSON.stringify(outputData, null, 1))
+}
+
